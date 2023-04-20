ee21a781e05ba7a51dd9edd9a3f19d2531a62d23
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-20 13:51:04 +0530

    Established DB connection

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db');
 
 // Load the config file (where our global variables are present)
 dotenv.config({ path: './config/config.env' })
+
+connectDB();
 
 // Initialize our app with express
 const app = express()
