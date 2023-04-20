c37381851ee7d4a6819b849cddb6e1d106dbfc26
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-20 12:52:41 +0530

    Added config file, modified app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require('express')
 const dotenv = require('dotenv')
+
+// Load the config file (where our global variables are present)
+dotenv.config({ path: './config/config.env' })
+
+// Initialize our app with express
+const app = express()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
