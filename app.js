bfd8b385fc78937963993761beae2d7760b38f5f
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-21 10:42:50 +0530

    Set up view engine and added routes

--- app.js
+++ app.js
@@ -18,8 +18,11 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 // Setting the view engine with handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', exphbs.engine(({ defaultLayout: 'main', extname: '.hbs' })))
 app.set('view engine', '.hbs')
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
