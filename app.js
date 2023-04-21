595735c81e17a503d320f438e46e6eee8bc66ea7
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-21 10:08:05 +0530

    Modified app.js, added morgan and view engine-handlebars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db');
 
 // Load the config file (where our global variables are present)
@@ -9,6 +11,15 @@ connectDB();
 
 // Initialize our app with express
 const app = express()
+
+// Run morgan in development mode to see request logs
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Setting the view engine with handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
