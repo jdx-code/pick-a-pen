2726188c3b50703ad1b6e0923c163e98e9610a99
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-22 22:59:24 +0530

    Added Google authentication using passport

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const path = require('path')
 const connectDB = require('./config/db');
 
 // Load the config file (where our global variables are present)
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -22,11 +27,24 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs.engine(({ defaultLayout: 'mainLayout', extname: '.hbs' })))
 app.set('view engine', '.hbs')
 
+// Sessions
+app.use(session({
+    secret: 'some secret here',
+    resave: false,              // means we don't want to save a session if nothing is modified
+    saveUninitialized: false,   // means don't create a session until a something is stored 
+}))
+
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
