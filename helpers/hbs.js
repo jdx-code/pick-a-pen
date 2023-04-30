17f57a0b77f0054428c812c46463c74edb3cfb17
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-30 16:44:00 +0530

    Edit stories button added

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -19,6 +19,30 @@ module.exports = {
 
     stripTags: function (input) {
     return input.replace(/<(?:.|\n)*?>/gm, '');
-    }
+    },    
 
+    editIcon: function (storyUser, loggedUser, storyId, floating = true) {
+        
+        if (storyUser._id.toString() === loggedUser._id.toString()) {
+          if (floating) {
+            return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+          } else {
+            return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`
+          }
+        } else {
+          return ''
+        }
+      },
+
+    // editIcon: function (storyUser, loggedUser, storyId, floating = true) {
+    //   if (storyUser._id.toString() === loggedUser._id.toString()) {
+    //     const editIconHtml = floating ? 
+    //       `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>` :
+    //       `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
+    //     return new Handlebars.SafeString(editIconHtml);
+    //   } else {
+    //     return '';
+    //   }
+    // }
+    
 }
